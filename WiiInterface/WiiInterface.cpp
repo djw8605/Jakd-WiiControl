2385d22297ea6f6736bd41b3175ee00d52a0112d
dweitzel <dweitzel@513aecad-5a16-4a60-b203-4838244ae98c>
2009-01-13 21:42:42 +0000

    git-svn-id: svn://derek.unl.edu/derek/SeniorDesign/WiiControl@340 513aecad-5a16-4a60-b203-4838244ae98c

--- WiiInterface/WiiInterface.cpp
+++ WiiInterface/WiiInterface.cpp
@@ -16,4 +16,85 @@ void StartWiiMotes()
 	
 }
 
+void handle_event(struct wiimote_t* wm)
+{
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_A))
+		printf("A pressed\n");
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_B))
+		printf("B pressed\n");
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_UP)) {
+		printf("UP pressed\n");
+	}
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_DOWN)) {
+		printf("DOWN pressed\n");
+	}
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_LEFT))
+		printf("LEFT pressed\n");
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_RIGHT))
+		printf("RIGHT pressed\n");
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_MINUS))
+		printf("MINUS pressed\n");
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_PLUS))
+		printf("PLUS pressed\n");
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_ONE))
+		printf("ONE pressed\n");
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_TWO))
+		printf("TWO pressed\n");
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_HOME))
+		printf("HOME pressed\n");
+
+	if (IS_JUST_PRESSED(wm, WIIMOTE_BUTTON_MINUS))
+		wiiuse_set_ir(wm, 0);
+
+	if (IS_JUST_PRESSED(wm, WIIMOTE_BUTTON_PLUS))
+		wiiuse_set_ir(wm, 1);
+	
+	
+	
+}
+
+
+void WiiMotesInstalled()
+{
+	
+	
+	
+}
+
+
+void ProcessWiiEvents()
+{
+	if (wiiuse_poll(wiimotes, MAX_WIIMOTES))
+	{
+		int i = 0;
+		for(; i< MAX_WIIMOTES; i++)
+		{
+			switch (wiimotes[i]->event) 
+			{
+				case WIIUSE_EVENT:
+					handle_event(wiimotes[i]);
+					break;
+				case WIIUSE_READ_DATA:
+			    case WIIUSE_NONE:
+			    case WIIUSE_STATUS:
+			    case WIIUSE_CONNECT:
+			    case WIIUSE_DISCONNECT:
+			    case WIIUSE_UNEXPECTED_DISCONNECT:
+			    case WIIUSE_NUNCHUK_INSERTED:
+			    case WIIUSE_NUNCHUK_REMOVED:
+			    case WIIUSE_CLASSIC_CTRL_INSERTED:
+			    case WIIUSE_CLASSIC_CTRL_REMOVED:
+			    case WIIUSE_GUITAR_HERO_3_CTRL_INSERTED:
+			    case WIIUSE_GUITAR_HERO_3_CTRL_REMOVED:
+			    	break;
+			}
+			
+		}
+		
+		
+	}
+	
+	
+}
+
 
---
+++ wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+export LD_LIBRARY_PATH=wiiuse/src/release-x86_64-redhat-linux:$LD_LIBRARY_PATH
+
+./wiicontrol
+
+
+
+
