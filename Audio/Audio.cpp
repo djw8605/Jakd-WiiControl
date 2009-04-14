456db64cb273d284214901ad63a3394d5785a1ea
dweitzel <dweitzel@513aecad-5a16-4a60-b203-4838244ae98c>
2009-04-14 03:26:49 +0000

    Made the audio buffers smaller to play faster, and more on time.
    git-svn-id: svn://derek.unl.edu/derek/SeniorDesign/WiiControl@463 513aecad-5a16-4a60-b203-4838244ae98c

--- Audio/Audio.cpp
+++ Audio/Audio.cpp
@@ -15,7 +15,7 @@ Audio::Audio()
     int audio_rate = 22050;
     Uint16 audio_format = AUDIO_S16SYS;
     int audio_channels = 2;
-    int audio_buffers = 4096;
+    int audio_buffers = 1024;
 
     if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) != 0) {
             fprintf(stderr, "Unable to initialize audio: %s\n", Mix_GetError());
